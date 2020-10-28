6b05e0ff2c32f17e0d15a2390e9da9fdf5e4b8ad
Mo <mo-zag@users.noreply.github.com>
2020-10-28 04:19:20 +0000

    Update autocomplete.js
    auto complete now display selected option to the user.

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -20,23 +20,39 @@ var pmp_auto_complete = {
 
   initiante_auto_complete: function() {
     var organisation_input = jQuery('#organisation');
+    jQuery('.clear-selected').hide();
     organisation_input.attr('type', 'hidden');
     var autoComplete = accessibleAutocomplete({
       element: document.querySelector('#my-autocomplete-container'),
       id: 'my-autocomplete',
       source: pmp_auto_complete.query,
-      name: 'anything[organisation]',
+      name: 'anything[organisation_auto_complete]',
       minLength: 3,
       defaultValue: organisation_input.attr('value'),
+      tNoResults: function() {
+       return  "too many results";
+      },
       onConfirm: function (query) {
         organisation_input.attr('value', query);
+        jQuery('#selected-autocomplete-option p span').text(query);
+        jQuery('.clear-selected').show();
       },
     });
+  },
+  clear_selected: function() {
+    jQuery('.clear-selected').on('click', function(e) {
+      e.preventDefault();
+        jQuery('#organisation').attr('value', '');
+        jQuery('#selected-autocomplete-option p span').text('');
+        jQuery('.autocomplete__input').val('')
+        jQuery('.clear-selected').hide();
+    })
   }
 }
 
 window.onload = function () {
   if($('#my-autocomplete-container').length > 0) {
     pmp_auto_complete.initiante_auto_complete();
+    pmp_auto_complete.clear_selected();
   }
 }
