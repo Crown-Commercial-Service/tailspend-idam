c6b58b5ad23dd4c17a100c8dbfce2f4136abd770
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2020-11-18 14:50:48 +0000

    Put error summary on all pages
    Add JavaScript which will focus on links
    
    Update JavaScript so that it works better
    
    Fix issue with organisation
    
    Remove unused methods and partials

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 require('jquery')
 require("packs/cp-sign-in")
 require('packs/autocomplete')
+require('packs/error-links')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
