0b16625320c4963416d1308ed197b1405e6333c1
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-02-02 17:02:45 +0000

    Fix issue with ga cookies

--- app/javascript/packs/cookie-policy.js
+++ app/javascript/packs/cookie-policy.js
@@ -1,36 +1,39 @@
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
-document.addEventListener('DOMContentLoaded', function() {
+const removeGACookies = () => {
+  const cookieList = Object.keys(Cookies.get());
+  const gaCookieList = [];
+
+  for (let i = 0; i < cookieList.length; i++) {
+    const cookieName = cookieList[i];
+
+    if (cookieName.startsWith('_ga')
+        || cookieName.startsWith('_gi')
+        || cookieName.startsWith(' _ga')
+        || cookieName.startsWith(' _gi')) gaCookieList.push(cookieName);
+  }
+
+  gaCookieList.forEach((cookieName) => Cookies.remove(cookieName));
+};
+
+document.addEventListener('DOMContentLoaded', () => {
   if (Cookies.get('pmp_cookie_settings_viewed') === 'true') $('#cookie-consent-container').hide();
   if (Cookies.get('pmp_google_analytics_enabled') !== 'true') removeGACookies();
 });
 
-const removeGACookies = function() {
-  let cookieList = Object.keys(Cookies.get());
-  let gaCookieList = [];
-
-  for(let i = 0; i < cookieList.length; i++){
-    let cookieName = cookieList[i];
-    
-    if (cookieName.startsWith('_ga') || cookieName.startsWith('_gi')) gaCookieList.push(cookieName);
-  }
-
-  gaCookieList.forEach(cookieName => Cookies.remove(cookieName));
-}
-
-$(function() {
-  $('#accept-all-cookies').on('click', function(e) {
+$(() => {
+  $('#accept-all-cookies').on('click', (e) => {
     e.preventDefault();
 
-    Cookies.set('pmp_cookie_settings_viewed', 'true', { expires: 365 } );
-    Cookies.set('pmp_google_analytics_enabled', 'true', { expires: 365 } );
+    Cookies.set('pmp_cookie_settings_viewed', 'true', { expires: 365 });
+    Cookies.set('pmp_google_analytics_enabled', 'true', { expires: 365 });
     $('#cookie-options-container').hide();
     $('#cookies-accepted-container').show();
   });
 
-  $('#save-cookie-settings-button').on('click', function() {
+  $('#save-cookie-settings-button').on('click', () => {
     if ($('input[name=ga_cookie_usage]:checked').val() === 'true') {
-      Cookies.set('pmp_google_analytics_enabled', 'true', { expires: 365 } );
+      Cookies.set('pmp_google_analytics_enabled', 'true', { expires: 365 });
     } else {
       Cookies.remove('pmp_google_analytics_enabled');
       removeGACookies();
