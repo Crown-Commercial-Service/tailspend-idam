bc618d69212bcbe4e316b4357e116eef58dddb92
Mark Henwood <mcbhenwood@icloud.com>
2023-09-18 16:19:21 +0100

    Add an env var to block Arask operations
    See
    https://crowncommercialservice.atlassian.net/browse/GMBP-126

--- config/initializers/arask.rb
+++ config/initializers/arask.rb
@@ -1,6 +1,11 @@
-Arask.setup do |arask|
-  arask.create job: 'SalesforceImportJob', cron: '0 */1 * * *', run_first_time: true if Rails.env.production?
+if ENV.fetch('PREVENT_ARASK', '0') != '1'  # Set to '1' to disable Arask job setup
+  Rails.logger.info 'Setting up Arask'
+  Arask.setup do |arask|
+    arask.create job: 'SalesforceImportJob', cron: '0 */1 * * *', run_first_time: true if Rails.env.production?
 
-  # On exceptions, send email with details
-  arask.on_exception email: 'CSI@crowncommercial.gov.uk'
+    # On exceptions, send email with details
+    arask.on_exception email: 'CSI@crowncommercial.gov.uk'
+  end
+else
+  Rails.logger.info 'Skipping Arask setup'
 end
