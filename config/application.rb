9bdef47a2c622e0d8e0ae57c6f77986c0774512c
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2023-10-25 11:34:30 +0100

    Run the rails mirgrate job and make any neccessery changes.
    Add tests for controllers that were missing them.

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,15 @@ Bundler.require(*Rails.groups)
 module TailspendIdam
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
     # config.web_console.whitelisted_ips = ''
+
+    config.active_support.cache_format_version = 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
