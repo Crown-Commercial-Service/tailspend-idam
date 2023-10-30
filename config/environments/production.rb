83334e3868ef29f5238002ae734f153297fd2c47
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2023-10-30 15:28:49 +0000

    Allow the healthcheck endpoint to ignore the SSL config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,12 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.ssl_options = {
+    redirect: {
+      exclude: ->(request) { request.path.include?('health_check') }
+    }
+  }
+
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
