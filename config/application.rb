ea9fd64d581689b7ca5950c3231d0f2739d6e36a
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2023-12-14 09:04:26 +0000

    Remove config related to GPaaS as we do not want it to be used anymore and may cause confusion

--- config/application.rb
+++ config/application.rb
@@ -40,28 +40,6 @@ module TailspendIdam
     # the framework and any gems in your application.
 
     config.before_configuration do
-      begin
-        vcap_services = JSON.parse(ENV.fetch('VCAP_SERVICES', nil))
-        ENV['CCS_DEFAULT_DB_HOST'] = vcap_services['postgres'][0]['credentials']['host'].to_s
-        ENV['CCS_DEFAULT_DB_PORT'] = vcap_services['postgres'][0]['credentials']['port'].to_s
-        ENV['CCS_DEFAULT_DB_NAME'] = vcap_services['postgres'][0]['credentials']['name'].to_s
-        ENV['CCS_DEFAULT_DB_USER'] = vcap_services['postgres'][0]['credentials']['username'].to_s
-        ENV['CCS_DEFAULT_DB_PASSWORD'] = vcap_services['postgres'][0]['credentials']['password'].to_s
-
-        vcap_application = JSON.parse(ENV.fetch('VCAP_APPLICATION', nil))
-        ENV['ALLOWED_HOST_DOMAINS'] = vcap_application['application_uris'].join(',').to_s
-      rescue StandardError
-        # Rails.logger.debug e
-      end
-
-      Rails.application.credentials.config.each do |key, value|
-        next if key.to_s != ENV['SERVER_ENV_NAME'].to_s
-
-        value.each do |env_key, env_value|
-          ENV[env_key.to_s] = env_value.to_s
-        end
-      end
-
       config.exceptions_app = routes
     end
   end
