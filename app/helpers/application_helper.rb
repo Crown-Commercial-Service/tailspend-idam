4ce9cce2653c8c66591458b265a9340151a8b44f
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2024-07-01 16:27:36 +0100

    Replace existing code with new components from ccs-frontend.
    Specifically:
    - Remove types module for ccs-frontend as types are now provided in the package
    - Replace “Contact us” and “Password strength” with the new components from CCS Frontend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,35 @@ module ApplicationHelper
       !current_cookie_preferences.is_a?(Hash) || current_cookie_preferences.empty? ? TailspendIdam.default_cookie_options : current_cookie_preferences
     end
   end
+
+  def password_strength(password_id)
+    ccs_password_strength(
+      password_id,
+      [
+        {
+          type: :length,
+          value: 10,
+          text: I18n.t('common.passten')
+        },
+        {
+          type: :uppercase,
+          text: I18n.t('common.passcap')
+        },
+        {
+          type: :lowercase,
+          text: I18n.t('common.passlower')
+        },
+        {
+          type: :number,
+          text: I18n.t('common.passnum')
+        },
+        {
+          type: :symbol,
+          value: "=+\\-^$*.[\\]{}()?\"!@#%&/\\\\,><':;|_~`",
+          text: I18n.t('common.passsym')
+        }
+      ],
+      classes: 'govuk-!-margin-left-2'
+    )
+  end
 end
